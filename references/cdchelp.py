a3fb44fda132280ea1268a9bf398ea018cbd431c
mkwasnica <mkwasnica@hotmail.com"git config --global user.email mkwasnica@hotmail.com>
2020-03-21 20:28:26 -0400

    pct_bplot fxn added

--- references/cdchelp.py
+++ references/cdchelp.py
@@ -9,4 +9,11 @@ def val_check(data_frame, column_name = str):
 
 
 
- 
+ def pct_bplot(dataframe, group = str, target = 'admit_NICU', column_title = ['Y','N'] ):
+    df1 = dataframe.groupby([group])[[target]].count()
+    df2 =  dataframe.groupby([group])[target].value_counts().unstack()
+    df2 = df2.reindex(columns=column_title)
+    df3 = pd.merge(df1,df2, left_index = True, right_index = True)
+    pct_df = pd.DataFrame(list(map(lambda x: df3[x]/df3[target] * 100, df3.columns[1:])))
+    pct_df = pct_df.T
+    pct_df.plot(kind = 'bar', stacked = True)
